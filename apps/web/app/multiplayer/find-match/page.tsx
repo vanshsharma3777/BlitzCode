4ea30a9497aa13ea555e79574ab8683da5ed3aa3
Vansh Sharma <sharmavansh.3777@gmail.com>
2026-03-08 11:36:36 +0530

    added hovering property in the fields of the configuration

---
+++ apps/web/app/multiplayer/find-match/page.tsx
@@ -0,0 +1,7 @@
+export default function FindMatch(){
+    return(
+        <div>
+            hello world
+        </div>
+    )
+}
--- apps/web/components/atoms/TopicsCard.tsx
+++ apps/web/components/atoms/TopicsCard.tsx
@@ -7,7 +7,7 @@ selected: string
 export default function TopicsCard({field , selected }: Props ){
     console.log("selected: ", selected)
     return (
-        <div className={`flex justify-center bg-bg px-14 py-2 rounded-md my-2 mr-2 border-2 border-border hover:border-accent ${selected.toLowerCase() === field.toLowerCase() ?"border-[#5E6AD2]" : null }`}>
+        <div className={`flex justify-center bg-bg px-14 py-2 rounded-md my-2 mr-2 border-2 border-border hover:border-accent transition-all duration-200 ease-in-out hover:scale-105 ${selected.toLowerCase() === field.toLowerCase() ? "border-[#5E6AD2] transition-all duration-200 ease-in-out scale-105" : null }`}>
             {field}
         </div>
     )
