67d5f84d594088f0ce40d5059764176d5cc48f3b
Vansh Sharma <sharmavansh.3777@gmail.com>
2026-04-23 18:13:50 +0530

    removed bun server from ws

--- apps/websockets/src/index.ts
+++ apps/websockets/src/index.ts
@@ -1,17 +1,9 @@
-import { serve } from "bun"
 import { WebSocketServer } from 'ws';
-import { INIT_GAME } from './message.js';
 import { GameManager } from './gameManager.js';
 
 const PORT = Number(process.env.PORT) || 3001;
 
 const gameManager = new GameManager()
-const server = serve({
-  port: PORT,
-  fetch(req) {
-    return new Response("Worker running 🚀");
-  }
-});
 const wss = new WebSocketServer({ port: 8080 });
 wss.on("connection", (socket) => {
   console.log("Connected");
